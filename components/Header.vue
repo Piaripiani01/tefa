e070e2e870bf77a1967a42f1abd04d4138cc879e
Piaripiani01 <141712563+Piaripiani01@users.noreply.github.com>
2024-03-14 14:44:06 +0700

    Add files via upload

---
+++ components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="row p-3 text-white content" >
+        <div class="col-lg-1">
+            <img src="../assets/img/LOGO-SMK4 (1) (1).png" alt="logo smkn 4">
+        </div>
+
+        <div class="col-lg-11"> 
+            <h1>PERPUSTAKAAN DIGITAL</h1>
+            <address>
+                SMKN 4 TASIKMALAYA
+                JL.DEPOK,SUKAMENAK,PURBARATU
+            </address>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+img {
+    width: 90px;
+}
+.content {
+    background-color: #89B6D6;
+}
+</style>
+
---
+++ pages/index.vue
@@ -0,0 +1,72 @@
+<template>
+    <div class="container-fluid">
+        <div class="row ny-5">
+            <div class="col-lg-6">
+            <nuxt-link to="/pengunjung/tambah">
+                <div class="card bg-pengunjung rounded-5">
+                    <div class="card-body">
+                        <h2>pengunjung</h2>
+                    </div>
+                </div>
+            </nuxt-link>
+            </div>
+
+            <div class="col-lg-6">
+                <nuxt-link to="/buku">
+                <div class="card bg-buku rounded-5">
+                    <div class="card-body">
+                        <h2>card buku</h2>
+                    </div>
+                </div>
+                </nuxt-link>
+            </div>
+        </div>
+        <h2>Statistik</h2>
+        <div class="row my-5">
+            <div class="col-lg-6">
+                <nuxt-link to="/pengunjung/index.vue">
+                <div class="card pengunjung rounded-5">
+                    <div class="card-body">
+                        <h2>0 Pengunjung</h2>
+                    </div>
+                </div>
+                </nuxt-link>
+            </div>
+
+            <div class="col-lg-6">
+                <nuxt-link to="/buku">
+                <div class="card buku rounded-5">
+                    <div class="card-body">
+                        <h2>50 Buku</h2>
+                    </div>
+                </div>
+                </nuxt-link>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.card {
+    height: 250px;
+    box-shadow: 1px 1px 10px hsl(0, 0%, 100%)
+}
+.card.bg-pengunjung {
+    background-image: url('../assets/img/pengunjung.jpeg');
+    background-repeat: no-repeat;
+    background-position: center center;
+    background-size: cover;
+}
+.card.bg-buku {
+    background: url('../assets/img/cari buku.jpg') no-repeat center center;
+    background-size: cover;
+}
+
+.pengunjung {
+    background-color: #89B6D6;
+}
+.buku {
+    background-color: #89B6D6;
+}
+
+</style>
